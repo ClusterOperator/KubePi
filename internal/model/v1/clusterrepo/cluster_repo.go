acc9ac2ff65fd42bd1d1f122cd03b0c59be98526
ai-liuys <ai-liuys@outlook.com>
2024-08-07 14:58:25 +0800

    :tada: init the project for cluster

---
+++ internal/model/v1/clusterrepo/cluster_repo.go
@@ -0,0 +1,10 @@
+package clusterrepo
+
+import v1 "github.com/ClusterOperator/kubepi/internal/model/v1"
+
+type ClusterRepo struct {
+	v1.BaseModel `storm:"inline"`
+	v1.Metadata  `storm:"inline"`
+	Cluster      string `json:"cluster"`
+	Repo         string `json:"repo"`
+}
